cbd3dde44396f226050df10af9f5258fbad09ebd
surajmendhe498 <surajmendhe470@gmail.com>
2025-01-29 19:25:01 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,27 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Create a new Sequelize instance
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
   process.env.DB_USER, 
   process.env.DB_PASSWORD, 
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql', // Database dialect (MySQL in this case)
+    host: process.env.DB_HOST, 
+    dialect: 'mysql',
+    logging: false, // Disable SQL query logging if not needed
   }
 );
 
-// Test the database connection
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connected to MySQL Database');
+    console.log('Connected to MySQL Database.');
   })
   .catch((err) => {
-    console.error('Unable to connect to the database:', err);
+    console.error('Error connecting to MySQL', err);
   });
 
 
-  
 module.exports = sequelize;
+
